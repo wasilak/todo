933bc1a40ddde22e2b266398b9b1b77f3e7a032f
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2013-12-26 19:26:48 +0100

    removed config file from repository

--- bootstrap.php
+++ bootstrap.php
@@ -9,9 +9,4 @@ define('MODELS_PATH', LIB_PATH . "/Wasilak/Models");
 
 if (PHP_SAPI != 'cli') define('URI', rtrim($_SERVER['REQUEST_URI'], '/'));
 
-define('DB_DRIVER', 'pdo_mysql');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'todo');
-define('DB_DEV_MODE', false);
-define('DB_HOST', '127.0.0.1');
+include 'config.php';
